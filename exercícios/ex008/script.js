9c973de919222c54325eb0725b3f660f02b8b207
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-01 10:42:44 -0300

    Exercícios 08 e 09

---
+++ exercícios/ex008/script.js
@@ -0,0 +1,6 @@
+let vel = 78.2;
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if(vel > 60){
+    console.log("Voçê ultrapassou a velocidade permitida, MULTADO!");
+}
+console.log("dirija sempre usando cinto de segurança");
