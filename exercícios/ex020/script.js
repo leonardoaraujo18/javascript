aebca6697608899e0c3479b8fe15457b25443e8f
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-04 19:33:11 -0300

    Exercício 020

--- exercícios/ex020/script.js
+++ exercícios/ex020/script.js
@@ -1,6 +1,6 @@
-function soma(n1,n2){
+function soma(n1=0, n2=0){
     return n1 + n2
 }
 
-let resultado = soma(10,5);
+let resultado = soma(25,5);
 console.log(`O resultado da soma é: ${resultado}`);
